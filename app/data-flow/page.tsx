af51e42a59a3610405babc985bfe792993fa36eb
Taha Kazmi <itplfirebase@gmail.com>
2025-04-21 17:09:54 +0530

    embeddedd the data flow chart in app

---
+++ app/data-flow/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <iframe width="100%" height="900px" src="https://miro.com/app/live-embed/uXjVIAfvrEQ=/?moveToViewport=-676,-501,1926,956&embedId=888063960818" frameBorder="0" scrolling="no" allow="fullscreen; clipboard-read; clipboard-write" allowFullScreen></iframe>
+    )
+}
+
+export default page
